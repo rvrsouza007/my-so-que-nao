ca0d175ae731ee27853fdbfda49fb5f7919a6796
Rhuan Souza <rhuanramos762@gmail.com>
2022-06-30 17:02:02 -0300

    async e await's

---
+++ tests/testeConexaoAsyncAwait.js
@@ -0,0 +1,23 @@
+// 1 - Importar o sequelize
+const sequelize = require('sequelize');
+
+// 2 - Elencar os dados para conexão com o BD
+const config = {
+    username:"root",
+    password:"vaiplaneta",
+    host:"localhost",
+    port:3306,
+    dialect:"mysql",
+    database:"osmaker"
+}
+
+// 3 - Criar a conexão com o banco!
+const conexao = new sequelize(config) 
+
+// 4 - Realizar uma consulta!!!
+let carregarFuncionarios =  async ()=>{
+    let resultado = await conexao.query("SElECT * FROM funcionarios")
+    console.log(resultado);
+}
+//Executar a função carregar funcionarios
+carregarFuncionarios();
